b412d8b6b84768a1715d8150a675bcf8e8480070
Brian den Ouden <brian.denouden@ctrlchain.com>
2026-03-10 16:05:16 +0100

    PrePare mainmenu

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _levelParent;
+
+    [SerializeField]
+    private GameObject _levelSelectPrefab;
+
+    private void Start()
+    {
+           
+    }
+}
